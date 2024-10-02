57fcf3fb2a5c3bccac5b92edee823737e3d56c5c
khireddinemahaline <khirozr@gmail.com>
2024-10-02 16:19:56 -0400

    promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,16 +1,17 @@
-import {uploadPhoto ,createUser } from './utils'
+import { uploadPhoto, createUser } from './utils';
+
 async function handleProfileSignup() {
-    try {
-        const [photoResponse, userResponse]  = await Promise.all(
-        [
-            uploadPhoto(),
-            createUser()
-        ]
+  try {
+    const [photoResponse, userResponse] = await Promise.all(
+      [
+        uploadPhoto(),
+        createUser(),
+      ],
     );
-    console.log(`Body: ${userResponse.firstName} ${userResponse.lastName}`);
-    } catch (error){
-        console.error("Signup system offline");
-    }
+    console.log(`${photoResponse.body}: ${userResponse.firstName} ${userResponse.lastName}`);
+  } catch (error) {
+    console.error('Signup system offline');
+  }
 }
 
-export default handleProfileSignup()
+export default handleProfileSignup;
--- 0x01-ES6_promise/try.js
+++ 0x01-ES6_promise/try.js
@@ -1,24 +1,24 @@
 // Function to use async/await for fetching data
 
 function fetchData() {
-    return new Promise((resolve, reject) =>{
-        const sucess = false;
-        if (sucess) {
-            const data = {age:23};
-            resolve(data);
-        } else {
-            reject(new Error("no data"))
-        }
-    })
-}
-    
-async function getData() {
-    try {
-        const result = await fetchData(); // Await the promise
-        console.log(result.age); // Log the success age
-    } catch (error) {
-        console.error("Error:", error.message); // Handle the error
+  return new Promise((resolve, reject) => {
+    const sucess = false;
+    if (sucess) {
+      const data = { age: 23 };
+      resolve(data);
+    } else {
+      reject(new Error('no data'));
     }
+  });
+}
+
+async function getData() {
+  try {
+    const result = await fetchData(); // Await the promise
+    console.log(result.age); // Log the success age
+  } catch (error) {
+    console.error('Error:', error.message); // Handle the error
+  }
 }
 
 // Calling the async function
