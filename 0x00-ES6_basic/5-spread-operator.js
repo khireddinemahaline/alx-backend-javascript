3cfaf70f7f81f2e4d6b190831d11073b86a56349
khireddinemahaline <khirozr@gmail.com>
2024-10-02 13:22:48 -0400

    add files

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,4 +1,4 @@
 export default function concatArrays(array1, array2, string) {
-  const new_array = [...array1, ...array2, ...string];
-  return new_array;
+  const newarray = [...array1, ...array2, ...string];
+  return newarray;
 }
