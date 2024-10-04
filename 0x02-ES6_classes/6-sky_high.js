8bad723ebbc21a83b64f684d4faa913431ee53ec
khireddinemahaline <khirozr@gmail.com>
2024-10-04 07:09:08 -0400

    symbole

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,22 +1,15 @@
 import Building from './5-building';
 
 class SkyHighBuilding extends Building {
-  constructor(floors, sqft) {
+  constructor(sqft, floors) {
     super(sqft);
-    this.floors = floors;
+    this._floors = floors;
   }
 
   get floors() { return this._floors; }
 
-  set floors(value) {
-    if (typeof value !== 'number') {
-      throw new Error('floor mus be a number');
-    }
-    this._floors = value;
-  }
-
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
 export default SkyHighBuilding;
