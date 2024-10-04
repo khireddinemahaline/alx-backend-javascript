bd7c6533b1f5faef2c09194ed1b69cf97657d5c7
khireddinemahaline <khirozr@gmail.com>
2024-10-04 04:57:28 -0400

    test

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,14 @@
+class Building {
+    constructor(sqft) {
+        if (new.target === Building) {
+            throw new Error("Class extending Building must override evacuationWarningMessage");
+        }
+        this._sqft = sqft;
+    }
+
+    get sqft() {return this._sqft}
+
+    evacuationWarningMessage() {
+    }
+}
+export default Building
