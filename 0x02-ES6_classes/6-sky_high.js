41e75642f65ed0178c87daf3fa86031a9ab0dc24
khireddinemahaline <khirozr@gmail.com>
2024-10-04 05:25:19 -0400

    inhertince

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,10 +3,17 @@ import Building from './5-building';
 class SkyHighBuilding extends Building {
   constructor(floors, sqft) {
     super(sqft);
-    this._floors = floors;
+    this.floors = floors;
   }
 
   get floors() { return this._floors; }
+
+  set floors(value) {
+    if (typeof value !== 'number') {
+      throw new Error('floor mus be a number');
+    }
+    this._floors = value;
+  }
 
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors`;
